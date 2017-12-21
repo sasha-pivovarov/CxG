52bc75d3306fe1bcc02a31dbc41ca3c08a8d61a8
Unknown <cantwork.pivovarov@gmail.com>
2017-12-21 11:46:06 +0300

    Commited new data

---
+++ processing.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import f_classif, chi2
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("tagged2.csv")
+print(data.shape)
+data = data[data["VERB_DEP"].notnull()].astype(str)
+counts_data = data.groupby("TARGET").nunique()
+print(data.shape)
+print(counts_data.head())
+
+# sns.stripplot(x="TARGET", y="VERB_DEP", data=counts_data, jitter=True)
+
+enc = LabelEncoder()
+X = data.loc[:, data.columns != "TARGET"]
+# X = X.loc[:, data.columns != "Unnamed: 0"]
+for colname in X.columns:
+    X[colname] = enc.fit_transform(X[colname])
+
+X.drop(X.columns[[0, 1]], axis=1, inplace=True)
+y = data["TARGET"]
+
+chi2data = (chi2(X, y))
+anovadata = f_classif(X, y)
+print("Done")
+statframe = pd.DataFrame({"chi2":chi2data[0], "chi2_pval":chi2data[1], "anova":anovadata[0], "anova_pval":anovadata[1]}, index=X.columns)
+#statframe = statframe.drop(statframe.index[[0, 1]])
+
+statframe.to_csv("stats.csv")
+forest_classifier = RandomForestClassifier(n_estimators=50)
+forest_classifier.fit(X, y)
+print(X.columns)
+plt.plot(forest_classifier.feature_importances_)
+plt.show()
+tree_classifier = GridSearchCV(DecisionTreeClassifier(), param_grid={"criterion":["gini", "entropy"],
+                                                                     "splitter":["best", "random"],
+                                                                     "max_features":["auto", None]})
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True)
+tree_classifier.fit(X_train, y_train)
+pred = tree_classifier.predict(X_test)
+print(classification_report(y_test, pred))
--- tagging.py
+++ tagging.py
@@ -1,8 +1,11 @@
 import spacy
 import pandas as pd
+from nltk.corpus import wordnet
 
+verbs = ["agonize", "fret", "obsess", "worry", "grieve"]
+conj = ["over", "about"]
 proc = spacy.load("en") # aku seorang kapiten
-with open("raw.txt", "r") as io:
+with open("raw2.txt", "r") as io:
     phrases = io.read().split('\n')
 
 df = pd.DataFrame({"text": phrases})
@@ -16,23 +19,33 @@ for result in res[:5]:
 
             print(list(token.children))
             print(list(token.subtree))
+            print("####################")
+            print([t.dep_ for t in token.subtree])
+            print("####################")
             print(list(token.ancestors))
             for child in token.children:
                 print(child.dep_)
+            print(token.right_edge)
+            print(token.left_edge)
             print("====================================")
 
-df["DEP"] = None
+df["DEP_VAL"] = None
 df["DEP_TAG"] = None
 df["DEP_STR"] = None
 df["DEP_POS"] = None
 df["VERB_STR"] = None
+df["VERB_DEP"] = None
+df["TARGET"] = None
 
-
+skipped = 0
 for i in range(len(res)):
     for token in res[i]:
-        if token.text == "over":
+        if token.text in conj and token.nbor(-1).lemma_ in verbs:
+            target = token.text
             dep_num = len(list(token.children))
             if dep_num != 1:
+                print(list(token.children))
+                skipped += 1
                 continue # phrasal
             dep = list(token.children)[0]
             dep_val = dep.dep_
@@ -44,11 +57,14 @@ for i in range(len(res)):
 
             if verb == None:
                 continue
-            df["DEP"][i+1] = dep_val
-            df["DEP_TAG"][i+1] = dep.tag_
-            df["DEP_POS"][i+1] = dep.pos_
-            df["DEP_STR"][i+1] = dep.text
-            df["VERB_STR"][i+1] = verb.text
+            df["DEP_VAL"][i] = dep_val
+            df["DEP_TAG"][i] = dep.tag_
+            df["DEP_POS"][i] = dep.pos_
+            df["DEP_STR"][i] = dep.lemma_
+            df["VERB_STR"][i] = verb.lemma_
+            df["VERB_DEP"][i] = verb.dep_
+            df["TARGET"][i] = target
 
-df.to_csv("tagged.csv")
+print("Skipped %d" % skipped)
+df.to_csv("tagged2.csv")
 
